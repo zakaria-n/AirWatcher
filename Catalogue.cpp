93b757059c7647609067f4da82d34a13504710a9
Sophie Crowley <crowleso@tcd.ie>
2020-05-27 14:42:04 +0100

    Update Catalogue.cpp

--- Catalogue.cpp
+++ Catalogue.cpp
@@ -130,6 +130,6 @@ void Catalogue::setCleaners(string fileName)
 
 vector<float> Catalogue::getAverageQuality
 (float latitude, float longitude, float radius, time_t begin, time_t end) {
-  Area curr = Area(latitudeInput,longitudeInput,radius);
-  curr.getAverageQuality(timestampBegin, timestampEnd);
+  Area curr = Area(latitude,longitude,radius);
+  vector<float> averages = curr.getAverageQuality(begin, end);
 }
